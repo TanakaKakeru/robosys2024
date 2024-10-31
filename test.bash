444001bf416c21bf1d1c4d415be2aab17d955be1
Tanaka Kakeru <s23c1085lw@s.chibakoudai.jp>
2024-10-31 12:29:32 +0900

    add test.bush

--- test.bash
+++ test.bash
@@ -2,14 +2,16 @@
 
 
 ng () {
-	echo${1}行目が違うよ
+	echo ${1}行目が違うよ
 	res=1
 }
 
 res=0
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+[ "${out}" = 15.0 ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
+
+
