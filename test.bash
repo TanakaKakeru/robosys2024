aa2ff463377ea84854dd5076a7a34b8fef2d78da
Tanaka Kakeru <s23c1085lw@s.chibakoudai.jp>
2024-11-07 11:08:06 +0900

    add license test.bash

--- test.bash
+++ test.bash
@@ -1,5 +1,6 @@
-#!/bin/bash
-
+#!/bin/bash -xv
+# SPDX-FileCopyrightSPDX-FileCopyrightText: 2024 Kakeru Tanaka
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
 	echo ${1}行目が違うよ
@@ -8,8 +9,18 @@ ng () {
 
 res=0
 
+### NORMAL INPUT ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15.0 ] || ng "$LINENO"
+
+### STRANGEINPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
